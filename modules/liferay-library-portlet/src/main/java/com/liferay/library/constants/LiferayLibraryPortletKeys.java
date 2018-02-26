cbb591000ce03275b2439f5b1b0cc1cc7d8be9e3
hudakl <laszlo.hudak@liferay.com>
2018-02-26 12:26:14 +0100

    LRSUPPORT-20776 Create portlet module

---
+++ modules/liferay-library-portlet/src/main/java/com/liferay/library/constants/LiferayLibraryPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.library.constants;
+
+/**
+ * @author Hudák László
+ */
+public class LiferayLibraryPortletKeys {
+
+	public static final String LiferayLibrary = "LiferayLibrary";
+
+}
